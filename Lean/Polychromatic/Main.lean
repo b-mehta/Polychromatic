087b15e5aed7b41cf16c6c66ff2e59ececd45464
Bhavik Mehta <bhavikmehta8@gmail.com>
2025-11-21 02:22:05 +0100

    update\

--- Lean/Polychromatic/Main.lean
+++ Lean/Polychromatic/Main.lean
@@ -2,7 +2,9 @@ import Polychromatic.Existence
 import Polychromatic.PolychromNumber
 import Polychromatic.Compute
 
-#check allC_289
+example : ∀ (a b c : ℤ), 0 < a → a < b → b < c → c < 289 →
+    HasPolychromColouring (Fin 3) {0, a, b, c} :=
+  suffices_flip _ (suffices_gcd _ (suffices_nat _ (allC_289)))
 
 -- ANCHOR: final
 theorem final_result (S : Finset ℤ) (hS : S.card = 4) :
